555468e7d26e3e42e79aae235af3a4b22130aeb0
Kinaraa <kinaradamayanti2@gmail.com>
2026-05-12 21:46:08 +0700

    Update main.js

--- login/js/main.js
+++ login/js/main.js
@@ -14,3 +14,79 @@
 });
 
 })(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+(function($) {
+
+	"use strict";
+
+	$(".toggle-password").click(function() {
+
+		$(this).toggleClass("fa-eye fa-eye-slash");
+
+		var input = $($(this).attr("toggle"));
+
+		if (input.attr("type") == "password") {
+			input.attr("type", "text");
+		} else {
+			input.attr("type", "password");
+		}
+
+	});
+
+})(jQuery);
+
+
+// LOGIN LOGOUT NAVBAR
+document.addEventListener("DOMContentLoaded", function () {
+
+    const username = localStorage.getItem("username");
+
+    const authButton = document.getElementById("authButton");
+
+    const userInfo = document.getElementById("userInfo");
+
+    // kalau elemen tidak ada, hentikan
+    if (!authButton || !userInfo) return;
+
+    if (username) {
+
+        // tampilkan username
+        userInfo.innerText = "Halo, " + username;
+
+        // ubah tombol jadi logout
+        authButton.innerText = "Logout";
+
+        authButton.href = "#";
+
+        authButton.addEventListener("click", function (e) {
+
+            e.preventDefault();
+
+            localStorage.removeItem("username");
+
+            location.reload();
+
+        });
+
+    } else {
+
+        userInfo.innerText = "Belum Login";
+
+        authButton.innerText = "Login";
+
+        authButton.href = "login/index.html";
+
+    }
+
+});
